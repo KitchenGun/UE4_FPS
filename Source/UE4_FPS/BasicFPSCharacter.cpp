b78e6255071110e6af02e164b4db44270355db84
KitchenGun <kitchengun124@gmail.com>
2021-05-23 22:24:16 +0900

    캐릭터 매쉬 랜더링 & 캐릭터 이동 코드 수정 & 파일 이름 수정
    2.8-캐릭터에 1 인칭 메시 추가
    OwnerNoSee같은 값들이 수정이 안되서 해당문제 수정 ref link (https://forums.unrealengine.com/t/mesh-setownernosee-question/32120)
    
    캐릭터 점프 이동 코드의 문제를 수정함
    FRotationMatrix(this->GetControlRotation()).GetScaledAxis(EAxis::Y) -> FVector MoveDir = this->GetActorRightVector(); 으로 변경하여 속도가 마우스를 돌려도 속도를 일정하게 수정하였다.
    
    파일이름 BasicFPSCharater -> BasicFPSCharacter로 수정

---
+++ Source/UE4_FPS/BasicFPSCharacter.cpp
@@ -0,0 +1,144 @@
+
+
+#include "BasicFPSCharacter.h"
+#include <Runtime/Engine/Classes/Engine/Engine.h>
+#include "Runtime/Engine/Classes/Components/InputComponent.h"
+#include "Runtime/Engine/Classes/Components/SkeletalMeshComponent.h"
+#include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
+
+// Sets default values
+ABasicFPSCharacter::ABasicFPSCharacter()
+	:
+	m_fMoveForwardSpeed(0.7f),
+	m_fMoveRightSpeed(0.7f),
+	m_bMoveForwardDir(false),
+	m_VMovementDirection(FVector().ZeroVector),
+	m_fMouseSensitivity(1.2f),
+	m_nJumpCounter(0),
+	m_nJumpMaxCounter(1),
+	m_fJumpHeight(400.0f)
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	//카메라 관련 코드
+	// Create a first person camera component.
+	m_oFPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(m_oFPSCameraComponent != nullptr);
+	// Attach the camera component to our capsule component.
+	m_oFPSCameraComponent->SetupAttachment((USceneComponent*)GetCapsuleComponent());
+	// Position the camera slightly above the eyes.
+	m_oFPSCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));//BaseEyeHeight
+	// Enable the pawn to control camera rotation.
+	m_oFPSCameraComponent->bUsePawnControlRotation = true;
+	//1인칭 mesh
+	m_oFPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(m_oFPSMesh != nullptr);
+	//자신에게만 보이도록 제작
+	m_oFPSMesh->SetOnlyOwnerSee(true);
+	m_oFPSMesh->SetupAttachment(m_oFPSCameraComponent);
+	//그림자 지우기
+	m_oFPSMesh->bCastDynamicShadow = false;
+	m_oFPSMesh->CastShadow = false;
+	//3인칭 모델링 안보이게 하기
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void ABasicFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GEngine)
+	{
+		// 5 초간 디버그 메시지를 표시합니다. (첫 인수인) -1 "Key" 값은 이 메시지를 업데이트 또는 새로고칠 필요가 없음을 나타냅니다.
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	}
+}
+
+void ABasicFPSCharacter::MoveForward(float a_fValue)
+{ 
+	//전진 이동중인지 확인용
+	if (a_fValue != 0)//입력이 없으면 실행안함
+	{
+		m_bMoveForwardDir = true;
+		// 어느 쪽이 전방인지 알아내어, 플레이어가 그 방향으로 이동하고자 한다고 기록합니다.
+		FVector MoveDir = this->GetActorForwardVector();
+		//대각선 속도 제어를 위한 기록용 x축 이동 기록
+		m_VMovementDirection.X = a_fValue * m_fMoveForwardSpeed;
+		AddMovementInput(MoveDir, a_fValue * m_fMoveForwardSpeed);
+	}
+	else
+	{
+		m_bMoveForwardDir = false;
+	}
+}
+void ABasicFPSCharacter::MoveRight(float a_fValue)
+{
+	if (a_fValue != 0)//입력이 없으면 실행안함
+	{
+		// 어느 쪽이 전방인지 알아내어, 플레이어가 그 방향으로 이동하고자 한다고 기록합니다.
+		FVector MoveDir = this->GetActorRightVector();
+		if (!m_bMoveForwardDir)
+		{
+			AddMovementInput(MoveDir, a_fValue * m_fMoveRightSpeed);
+		}
+		else
+		{
+			//대각선 속도 제어를 위한 기록용 y축 이동 기록
+			m_VMovementDirection.Y = a_fValue * m_fMoveRightSpeed;
+			MoveDir *= abs(SpeedLimit(GetActorForwardVector()+ GetActorRightVector()).Y);
+			AddMovementInput(MoveDir, a_fValue * m_fMoveRightSpeed);
+		}
+	}
+}
+FVector ABasicFPSCharacter::SpeedLimit(FVector a_moveDir)
+{
+	double numerator = sqrt(pow(abs(a_moveDir.X), 2) + pow(abs(a_moveDir.Y), 2));
+	double denominator = (abs(a_moveDir.X) + abs(a_moveDir.Y));
+	double velocityRatio = (numerator / denominator);
+	return a_moveDir* velocityRatio;
+}
+
+void ABasicFPSCharacter::Turn(float a_fValue)
+{
+	AddControllerYawInput(a_fValue * m_fMouseSensitivity);
+}
+void ABasicFPSCharacter::LookUp(float a_fValue)
+{
+	AddControllerPitchInput(a_fValue * m_fMouseSensitivity);
+}
+
+void ABasicFPSCharacter::DoJump()
+{//기존의 튜토리얼 방식이 아닌 다른 방식으로 구현
+	if (m_nJumpCounter < m_nJumpMaxCounter)
+	{
+		ACharacter::LaunchCharacter(FVector(0, 0, m_fJumpHeight),false,true);
+		m_nJumpCounter++;
+	}
+}
+
+void ABasicFPSCharacter::Landed(const FHitResult& Hit)
+{//지면에 닿을 경우 0으로 만듬
+	m_nJumpCounter = 0;
+}
+
+// Called every frame
+void ABasicFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABasicFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	//인풋 컴포넌트 없을시 종료
+	check(PlayerInputComponent);
+	//이동
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABasicFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABasicFPSCharacter::MoveRight);
+	//시선
+	PlayerInputComponent->BindAxis("Turn", this, &ABasicFPSCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &ABasicFPSCharacter::LookUp);
+	//점프
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABasicFPSCharacter::DoJump);
+}
+
---
+++ Source/UE4_FPS/BasicFPSCharacter.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "BasicFPSCharacter.generated.h"
+
+UCLASS()
+class UE4_FPS_API ABasicFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABasicFPSCharacter();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Landed(const FHitResult& Hit) override;
+public:
+	//이동
+	void MoveForward(float a_fValue);
+	void MoveRight(float a_fValue);
+	//시선
+	void Turn(float a_fValue);
+	void LookUp(float a_fValue);
+	FVector SpeedLimit(FVector a_moveDir);
+	//UFUNCTION은 블루프린트에 접근을 하게 해준다
+	UFUNCTION()
+	void DoJump();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+private:
+	//이동 관련 변수
+	UPROPERTY(EditAnywhere, Category = "MoveSpeed")
+	float m_fMoveForwardSpeed;
+	UPROPERTY(EditAnywhere, Category = "MoveSpeed")
+	float m_fMoveRightSpeed;
+	bool m_bMoveForwardDir;//전진중인지 확인용 변수
+
+	FVector m_VMovementDirection;
+	//시선 관련 변수
+	UPROPERTY(EditAnywhere, Category = "Sensitivity")
+	float m_fMouseSensitivity;//민감도
+	//점프 관련 변수
+	int m_nJumpCounter;
+	UPROPERTY(EditAnywhere, Category = "Jump")
+	int m_nJumpMaxCounter;
+	UPROPERTY(EditAnywhere, Category = "Jump")
+	float m_fJumpHeight;
+	//카메라 
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* m_oFPSCameraComponent;
+	//일인칭 mesh
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* m_oFPSMesh;
+
+};
